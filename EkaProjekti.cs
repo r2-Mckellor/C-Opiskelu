fd2306ee9b59b9994cc8a797115b7e7698336dd1
r2-Mckellor <78972356+r2-Mckellor@users.noreply.github.com>
2022-08-22 09:22:10 +0300

    Add files via upload

---
+++ EkaProjekti.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EkaProjekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hyvää päivää!");
+        }
+    }
+}
